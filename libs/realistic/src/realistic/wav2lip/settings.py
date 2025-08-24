cf8b911d66ff6488993fb219fd6e38ccd1569648
realtrevor15 <ttkien1503@gmail.com>
2025-08-24 09:20:43 +0700

    livetalking-dockercompose

--- libs/realistic/src/realistic/wav2lip/settings.py
+++ libs/realistic/src/realistic/wav2lip/settings.py
@@ -5,20 +5,13 @@ from base import BaseModel
 from text_to_speech import XTTSSettings
 
 class LipRealSettings(BaseModel):
-    W: int = 450
-    H: int = 450
-    fps: int = 20
-    idx: int = 0
-    batch_size: int = 16
-    model: str = 'wav2lip'
-    avatar_id: str = ''
-    customopt: list = []
-    customvideo_config: str = ''
+    W: int
+    H: int
+    fps: int
+    idx: int
+    batch_size: int
+    model: str
+    customopt: list
+    customvideo_config: str
     
-    xtts: XTTSSettings = XTTSSettings()
-    
-    # avatar: tuple[list, list, Any]
-    
-    # frame_list_cycle: list = []
-    # face_list_cycle: list = []
-    # coord_list_cycle: Any = []
+    xtts: XTTSSettings
--- libs/text_to_speech/src/text_to_speech/xtts/settings.py
+++ libs/text_to_speech/src/text_to_speech/xtts/settings.py
@@ -6,5 +6,5 @@ from typing import Any
 from queue import Queue
 
 class XTTSSettings(BaseModel):
-    REF_FILE: str = 'thang.wav'
-    TTS_SERVER: str = 'http://127.0.0.1:8002'
+    ref_file: str
+    tts_server: str
