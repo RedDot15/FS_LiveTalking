3750f038e557d94b9e236b0fb51a5602ce8ff16f
RedDot15 <minhtb03@gmail.com>
2025-09-11 10:17:26 +0700

    refactor mongodb & stop tracking uv.lock

--- libs/mongo_client/src/mongo_client/controller/conversation.py
+++ libs/mongo_client/src/mongo_client/controller/conversation.py
@@ -1,38 +1,40 @@
+from typing import Any
 from ..model import Conversation
 from bson.objectid import ObjectId
+from base import BaseService
+from pymongo.collection import Collection
 
-class ConversationHandler:
+class ConversationHandler(BaseService):
+    collection: Collection
 
     # Create new conversation
     def create_conversation(self, conversation: Conversation):
-        collection = self._get_collection("conversations")
-        result = collection.insert_one(conversation.__dict__)
+        result = self.collection.insert_one(conversation.__dict__)
         return result.inserted_id
 
     # Get all conversation by participants_hash with (created_at order: desc)
     def get_conversation_by_participants_hash(self, participants_hash: str):
-        collection = self._get_collection("conversations")
-        data = collection.find({"participants_hash": participants_hash}).sort("created_at", 1)
+        data = self.collection.find({"participants_hash": participants_hash}).sort("created_at", 1)
         return list(data)
 
     # Get conversation by id
     def get_conversation_by_id(self, conversation_id: str):
-        collection = self._get_collection("conversations")
-        data = collection.find_one({"_id": ObjectId(conversation_id)})
+        data = self.collection.find_one({"_id": ObjectId(conversation_id)})
         return data
     
     # Update conversation by id (can only update name)
     def update_conversation_by_id(self, conversation_id: str, updated_conversation_name: str):
-        collection = self._get_collection("conversations")
         update_data = {
             "name": updated_conversation_name,
         }
-        return collection.update_one(
+        return self.collection.update_one(
             {"_id": ObjectId(conversation_id)},
             {"$set": update_data}
         )
     
     # Delete conversation by id
     def delete_conversation_by_id(self, conversation_id: str):
-        collection = self._get_collection("conversations")
-        return collection.delete_one({"_id": ObjectId(conversation_id)})
+        return self.collection.delete_one({"_id": ObjectId(conversation_id)})
+    
+    def process(self, inputs: Any) -> Any:
+        raise NotImplementedError("This method is not used.")
