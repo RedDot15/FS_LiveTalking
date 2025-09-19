a15ac3d54704e771d47839d1bc9ff23f144b548b
RedDot15 <minhtb03@gmail.com>
2025-09-19 23:07:56 +0700

    Initial identity-service

---
+++ services/identity_service/src/identity_service/app/api/main.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+from app.api.routes import items, login, users
+from app.core.config import settings
+
+api_router = APIRouter()
+api_router.include_router(login.router)
+api_router.include_router(users.router)
+api_router.include_router(items.router)
+
