8a350afbe45c280a99bb00fe5ee91a8dba0fbc64
huskywannacry <ttkien1503@gmail.com>
2025-12-07 16:41:23 +0700

    feat: get request character api

--- services/character_service/src/character_service/api/routers/v1/__init__.py
+++ services/character_service/src/character_service/api/routers/v1/__init__.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 from .character_router import character_router
 from .request_router import request_router
+from .get_character_request_router import get_character_request_router
 
 __all__ = [
     'character_router',
     'request_router',
+    'get_character_request_router'
 ]
---
+++ services/character_service/src/character_service/api/routers/v1/get_character_request_router.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+from typing import Annotated
+
+from authorization.deps import CurrentToken, has_authority
+from authorization.model import TokenPayload
+from character_service.application.request_service import RequestInput, RequestRejectInput
+from fastapi import APIRouter, Depends
+from fastapi import Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi import File, Form, UploadFile
+from logger import get_logger
+from character_service.api.helpers.exception_handler import ExceptionHandler
+from character_service.application.get_request_character import GetRequestCharacterApplication
+
+from character_service.shared import get_settings
+from logger import get_logger
+
+settings = get_settings()
+logger = get_logger(__name__)
+
+get_character_request_router = APIRouter()
+
+@get_character_request_router.get('/requests')
+def get_character_request(
+    request: Request,
+    current_token: CurrentToken
+    ):
+    
+    exception_handler = ExceptionHandler(
+        logger=logger.bind(),
+        service_name=__name__,
+    )
+    
+    try:
+        request_character_service = GetRequestCharacterApplication(
+            settings=settings,
+            request=request
+        )
+    
+    except Exception as e:
+        return exception_handler.handle_exception(
+            e=f'Error during application initialization: {str(e)}',
+            extra={},
+        )
+        
+    try:
+        response = request_character_service.process()
+        
+    except Exception as e:
+        return exception_handler.handle_exception(e=str(e))
+    
+    return exception_handler.handle_success(
+        jsonable_encoder(
+            response,
+        )
+    )
